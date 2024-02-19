65d489eda18adfa621eda6b0782dd451e012e181
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-19 14:34:57 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -x
 export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
 apt-get -qq update
-DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install \
+DEBIAN_FRONTEND=noninteractive apt-get -q -y --no-install-recommends install \
   apache2 \
   ca-certificates \
   curl \
@@ -29,14 +29,14 @@ a2enmod \
  proxy \
  proxy_http
 
-cat /etc/apache2/apache2.conf
+cp /etc/apache2/apache2.conf /var/www/html/auth/apache2.txt
 
 curl -sSL -o /etc/apache2/sites-enabled/apache.conf https://github.com/tshr20180821/render-11/raw/main/apache.conf
 sed -i s/__RENDER_EXTERNAL_HOSTNAME__/"${RENDER_EXTERNAL_HOSTNAME}"/g /etc/apache2/sites-enabled/apache.conf
 
 htpasswd -c -b /var/www/html/.htpasswd "${BASIC_USER}" "${BASIC_PASSWORD}"
 chmod 644 /var/www/html/.htpasswd
-. /etc/apache2/envvars
+. /etc/apache2/envvars >/dev/null
 
 curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/start_after.sh
 
