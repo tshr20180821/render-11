b54e7414799683e32c29c46932aa54158528d16d
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-19 14:57:18 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -11,6 +11,7 @@ while true; do \
   done \
    && ss -anpt \
    && ps aux \
+   && ls -lang /tmp \
    && curl -sS -A "keep instance" -u "${BASIC_USER}":"${BASIC_PASSWORD}" https://"${RENDER_EXTERNAL_HOSTNAME}"/; \
 done &
 
