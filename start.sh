2e9bbc675c35e440c9a98a1f835280d1a3dfb738
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-19 13:53:45 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -30,20 +30,21 @@ a2enmod \
  proxy_http
 
 mv /etc/apache2/apache2.conf /etc/apache2/apache2.conf.org
+touch /etc/apache2/apache2.conf
 
-curl -sSL -o /etc/apache2/apache2.conf https://github.com/tshr20180821/render-11/raw/main/apache.conf
-sed -i s/__RENDER_EXTERNAL_HOSTNAME__/"${RENDER_EXTERNAL_HOSTNAME}"/g /etc/apache2/apache2.conf
+curl -sSL -o /etc/apache2/sites-enabled/apache.conf https://github.com/tshr20180821/render-10/raw/main/apache.conf
+sed -i s/__RENDER_EXTERNAL_HOSTNAME__/"${RENDER_EXTERNAL_HOSTNAME}"/g /etc/apache2/sites-enabled/apache.conf
 
 htpasswd -c -b /var/www/html/.htpasswd "${BASIC_USER}" "${BASIC_PASSWORD}"
 chmod 644 /var/www/html/.htpasswd
-. /etc/apache2/envvars >/dev/null 2>&1
+. /etc/apache2/envvars >/dev/null
 
 ls -lang /etc/apache2/
 
-curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/start_after.sh
+# curl -sSL -O https://github.com/tshr20180821/render-10/raw/main/start_after.sh
 
-chmod +x ./start_after.sh
+# chmod +x ./start_after.sh
 
-sleep 5s && ./start_after.sh &
+# sleep 5s && ./start_after.sh &
 
 exec /usr/sbin/apache2 -DFOREGROUND
