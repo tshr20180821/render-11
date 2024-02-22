17ea0f8d2c075f7930345ac87c9619e42cbf4278
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-22 14:19:38 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -21,7 +21,8 @@ echo "Password = ${MEGA_PASSWORD}" >>/root/.megarc
 FAHClient --help >/var/www/html/auth/fahclient.txt
 # FAHClient --lspci >/var/www/html/auth/lspci.txt
 
-megatools get /Root/fah.tar.gz
+megatools mkdir /Root/${RENDER_EXTERNAL_HOSTNAME}
+megatools get /Root/${RENDER_EXTERNAL_HOSTNAME}/fah.tar.gz
 tar xf ./fah.tar.gz
 rm -f ./fah.tar.gz
 
@@ -37,13 +38,13 @@ while true; do \
    && ps aux \
    && rm -f /tmp/fah.tar.gz \
    && tar -zcf /tmp/fah.tar.gz ./fah \
-   && megatools rm --no-ask-password /Root/fah.tar.gz | true \
-   && megatools put --no-ask-password --path /Root/fah.tar.gz /tmp/fah.tar.gz; \
+   && megatools rm --no-ask-password /Root/${RENDER_EXTERNAL_HOSTNAME}/fah.tar.gz | true \
+   && megatools put --no-ask-password --path /Root/${RENDER_EXTERNAL_HOSTNAME}/fah.tar.gz /tmp/fah.tar.gz; \
 done &
 
 while true; do \
   FAHClient -v --gpu=false --chdir=/app/fah --power=full --http-addresses=127.0.0.1:7396 --command-address=127.0.0.1 \
-   --max-packet-size=small --verbosity=5 --exit-when-done=true \
+   --max-packet-size=small --verbosity=5 --exit-when-done=true --checkpoint=3 \
     && rm -f /app/fah/logs/* \
-    && megatools rm --no-ask-password /Root/fah.tar.gz; \
+    && megatools rm --no-ask-password /Root/${RENDER_EXTERNAL_HOSTNAME}/fah.tar.gz; \
 done
