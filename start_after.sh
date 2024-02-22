7eb0f8ae89e640819e67e88424a9f8c850d2f900
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-22 12:05:56 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -18,21 +18,6 @@ echo "[Login]" >/root/.megarc
 echo "Username = ${MEGA_EMAIL}" >>/root/.megarc
 echo "Password = ${MEGA_PASSWORD}" >>/root/.megarc
 
-while true; do \
-  for i in {1..10}; do \
-    sleep 60s \
-     && echo "${i}"; \
-  done \
-   && ss -anpt \
-   && ps aux \
-   && ls -lang /app/fah \
-   && rm -f /tmp/fah.tar.gz \
-   && tar -zcf /tmp/fah.tar.gz ./fah \
-   && megatools rm --no-ask-password /Root/fah.tar.gz \
-   && megatools put --path /Root/fah.tar.gz /tmp/fah.tar.gz \
-   && curl -sS -A "keep instance" -u "${BASIC_USER}":"${BASIC_PASSWORD}" https://"${RENDER_EXTERNAL_HOSTNAME}"/; \
-done &
-
 FAHClient --help >/var/www/html/auth/fahclient.txt
 # FAHClient --lspci >/var/www/html/auth/lspci.txt
 
@@ -41,6 +26,21 @@ tar xf ./fah.tar.gz
 
 ls -lang
 ls -lang /app/fah/
+
+while true; do \
+  for i in {1..10}; do \
+    sleep 60s \
+     && echo "${i}"; \
+     && curl -sS -A "keep instance" -u "${BASIC_USER}":"${BASIC_PASSWORD}" https://"${RENDER_EXTERNAL_HOSTNAME}"/; \
+  done \
+   && ss -anpt \
+   && ps aux \
+   && ls -lang /app/fah \
+   && rm -f /tmp/fah.tar.gz \
+   && tar -zcf /tmp/fah.tar.gz ./fah \
+   && megatools rm --no-ask-password /Root/fah.tar.gz | true \
+   && megatools put --path /Root/fah.tar.gz /tmp/fah.tar.gz \
+done &
 
 while true; do \
   FAHClient -v --gpu=false --chdir=/app/fah --power=full --http-addresses=127.0.0.1:7396 --command-address=127.0.0.1 \
