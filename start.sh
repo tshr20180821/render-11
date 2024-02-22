8a1730cdf1616e05d962dfd92fe25ac9279f8298
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-22 14:12:16 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,49 +4,46 @@ set -x
 
 export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
-apt-get -qq update
-DEBIAN_FRONTEND=noninteractive apt-get -q -y --no-install-recommends install \
-  apache2 \
-  ca-certificates \
-  curl \
-  iproute2
+mkdir /app/fah
 
-a2dissite -q 000-default.conf
+curl -sSO https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/latest.deb
 
-mkdir -p /var/www/html/auth
+DEBIAN_FRONTEND=noninteractive apt-get install -y ./latest.deb
+rm ./latest.deb
 
-chown www-data:www-data /var/www/html/auth -R
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+  megatools
 
-echo '<HTML />' >/var/www/html/index.html
+echo "[Login]" >/root/.megarc
+echo "Username = ${MEGA_EMAIL}" >>/root/.megarc
+echo "Password = ${MEGA_PASSWORD}" >>/root/.megarc
 
-{ \
-  echo 'User-agent: *'; \
-  echo 'Disallow: /'; \
-} >/var/www/html/robots.txt
+FAHClient --help >/var/www/html/auth/fahclient.txt
+# FAHClient --lspci >/var/www/html/auth/lspci.txt
 
-a2enmod \
- authz_groupfile \
- proxy \
- proxy_http
+megatools get /Root/fah.tar.gz
+tar xf ./fah.tar.gz
+rm -f ./fah.tar.gz
 
-cp /etc/apache2/apache2.conf /var/www/html/auth/apache2.txt
+ls -lang
+ls -lang /app/fah/
 
-curl -sSL -o /etc/apache2/sites-enabled/apache.conf https://github.com/tshr20180821/render-11/raw/main/apache.conf
-sed -i s/__RENDER_EXTERNAL_HOSTNAME__/"${RENDER_EXTERNAL_HOSTNAME}"/g /etc/apache2/sites-enabled/apache.conf
+while true; do \
+  for i in {1..10}; do \
+    sleep 60s \
+     && curl -sS -A "${i}" -u "${BASIC_USER}":"${BASIC_PASSWORD}" https://"${RENDER_EXTERNAL_HOSTNAME}"/; \
+  done \
+   && ss -anpt \
+   && ps aux \
+   && rm -f /tmp/fah.tar.gz \
+   && tar -zcf /tmp/fah.tar.gz ./fah \
+   && megatools rm --no-ask-password /Root/fah.tar.gz | true \
+   && megatools put --no-ask-password --path /Root/fah.tar.gz /tmp/fah.tar.gz; \
+done &
 
-htpasswd -c -b /var/www/html/.htpasswd "${BASIC_USER}" "${BASIC_PASSWORD}"
-chmod 644 /var/www/html/.htpasswd
-. /etc/apache2/envvars >/dev/null
-
-ln -sfT /dev/stderr ${APACHE_LOG_DIR}/error.log
-ln -sfT /dev/stdout ${APACHE_LOG_DIR}/access.log
-
-curl -sSL -O https://github.com/tshr20180821/render-11/raw/main/start_after.sh
-
-chmod +x ./start_after.sh
-
-sleep 5s && ./start_after.sh &
-
-mkdir ${APACHE_RUN_DIR}
-
-exec /usr/sbin/apache2 -DFOREGROUND
+while true; do \
+  FAHClient -v --gpu=false --chdir=/app/fah --power=full --http-addresses=127.0.0.1:7396 --command-address=127.0.0.1 \
+   --max-packet-size=small --verbosity=5 --exit-when-done=true --checkpoint=3 \
+    && rm -f /app/fah/logs/* \
+    && megatools rm --no-ask-password /Root/fah.tar.gz; \
+done
