95fdc6f38fa5527c3dda3e9a0de1b6f3e0dfe75b
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-27 19:03:54 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -4,37 +4,6 @@ set -x
 
 export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
-subroutine01() {
-  ss -anpt
-  ps aux
-  du -hd 1 /app/fah
-  ls -lang /app/fah/
-  rm -f /tmp/fah.tar.xz
-  tar cfJ /tmp/fah.tar.xz ./fah
-  megatools rm --no-ask-password /Root/"${RENDER_EXTERNAL_HOSTNAME}"/fah.tar.gz
-  megatools rm --no-ask-password /Root/"${RENDER_EXTERNAL_HOSTNAME}"/fah.tar.xz
-  ls -lang /tmp/fah.tar.xz
-  megatools put --no-ask-password --path /Root/"${RENDER_EXTERNAL_HOSTNAME}"/fah.tar.xz /tmp/fah.tar.xz
-  # grep steps /app/fah/log.txt | tail -n 1 >/tmp/logtail.txt
-  cat /app/fah/log.txt | grep steps | tail -n 1 >/tmp/logtail.txt
-  if [ -f /tmp/logtail.txt.old ]; then
-    if [ "1" = "$(diff -q /tmp/logtail.txt.old /tmp/logtail.txt | grep -c differ)" ]; then
-      curl -sSH "Authorization: Bearer ${SLACK_TOKEN}" \
-        -d "text=${RENDER_EXTERNAL_HOSTNAME} $(cat /tmp/logtail.txt)" -d "channel=${SLACK_CHANNEL_01}" \
-        https://slack.com/api/chat.postMessage >/dev/null
-      sleep 1s
-      curl -sSH "Authorization: Bearer ${SLACK_TOKEN}" \
-        -d "text=${RENDER_EXTERNAL_HOSTNAME} $(cat /tmp/logtail.txt)" -d "channel=${SLACK_CHANNEL_02}" \
-        https://slack.com/api/chat.postMessage >/dev/null
-    fi
-  fi
-  cp -f /tmp/logtail.txt /tmp/logtail.txt.old
-}
-
-subroutine02() {
-  subroutine01 &
-}
-
 curl -sSO https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/latest.deb &
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
@@ -99,7 +68,7 @@ while true; do \
     sleep 60s; \
     time curl -sSw "%{time_total}\n" -A "${i}" -u "${BASIC_USER}":"${BASIC_PASSWORD}" -o /dev/null https://"${RENDER_EXTERNAL_HOSTNAME}"/; \
   done \
-   && subroutine02; \
+   && ./subroutine01.sh &; \
 done &
 
 while true; do \
