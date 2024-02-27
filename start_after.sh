82aa7f820e41f78e1343d990169801d21161d87d
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-02-27 10:46:58 +0900

    Update start_after.sh

--- start_after.sh
+++ start_after.sh
@@ -4,6 +4,18 @@ set -x
 
 export PS4='+(${BASH_SOURCE}:${LINENO}): '
 
+log_check() {
+  tail -n 1 /app/fah/log.txt >/tmp/logtail.txt
+  if [ -f /tmp/logtail.txt.old ]; then
+    if [ "1" = $(diff -q /tmp/logtail.txt.old /tmp/logtail.txt | grep -c differ) ]; then
+      curl -sS -H "Authorization: Bearer ${SLACK_TOKEN}" \
+        -d "text=${RENDER_EXTERNAL_HOSTNAME} $(cat /tmp/logtail.txt)" -d "channel=${SLACK_CHANNEL_02}" \
+        https://slack.com/api/chat.postMessage >/dev/null
+    fi
+  fi
+  cp -f /tmp/logtail.txt /tmp/logtail.txt.old
+}
+
 curl -sSO https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/latest.deb &
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
@@ -30,21 +42,14 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y ./latest.deb &
 if [ ! -z ${SLACK_TOKEN} ]; then
   RANK=$(curl -sS https://api.foldingathome.org/team/${FAH_TEAM_NUMBER} | jq '.rank')
 
-  curl -sS -X POST -H "Authorization: Bearer ${SLACK_TOKEN}" \
+  curl -sS -H "Authorization: Bearer ${SLACK_TOKEN}" \
     -d "text=RESTART ${RENDER_EXTERNAL_HOSTNAME} RANK:${RANK}" -d "channel=${SLACK_CHANNEL_01}" https://slack.com/api/chat.postMessage >/dev/null
 
   sleep 1s
 
-  curl -sS -X POST -H "Authorization: Bearer ${SLACK_TOKEN}" \
+  curl -sS -H "Authorization: Bearer ${SLACK_TOKEN}" \
     -d "text=RESTART ${RENDER_EXTERNAL_HOSTNAME} RANK:${RANK}" -d "channel=${SLACK_CHANNEL_02}" https://slack.com/api/chat.postMessage >/dev/null
 fi
-
-wait
-
-rm ./latest.deb
-
-FAHClient --help >/var/www/html/auth/fahclient.txt
-# FAHClient --lspci >/var/www/html/auth/lspci.txt
 
 megatools mkdir /Root/${RENDER_EXTERNAL_HOSTNAME}
 megatools get /Root/${RENDER_EXTERNAL_HOSTNAME}/fah.tar.gz
@@ -54,6 +59,12 @@ rm -f ./fah.tar.gz
 ls -lang /app/fah/
 touch /app/fah/log.txt
 ln -s /app/fah/log.txt /var/www/html/auth/log.txt
+
+wait
+
+rm ./latest.deb
+
+FAHClient --help >/var/www/html/auth/fahclient.txt
 
 while true; do \
   for i in {1..30}; do \
@@ -68,7 +79,8 @@ while true; do \
    && tar -zcf /tmp/fah.tar.gz ./fah \
    && megatools rm --no-ask-password /Root/${RENDER_EXTERNAL_HOSTNAME}/fah.tar.gz | true \
    && ls -lang /tmp/fah.tar.gz \
-   && megatools put --no-ask-password --path /Root/${RENDER_EXTERNAL_HOSTNAME}/fah.tar.gz /tmp/fah.tar.gz; \
+   && megatools put --no-ask-password --path /Root/${RENDER_EXTERNAL_HOSTNAME}/fah.tar.gz /tmp/fah.tar.gz \
+   && log_check; \
 done &
 
 while true; do \
